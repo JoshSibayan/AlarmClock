f556020df7f08a1dfa83529f2dd73db16b48a186
Josh Sibayan <jbsibaya@ucsc.edu>
2016-08-31 18:56:59 -0700

    Added notification features

--- app/src/main/java/comjoshsibayan/github/alarm/RingtonePlayingService.java
+++ app/src/main/java/comjoshsibayan/github/alarm/RingtonePlayingService.java
@@ -1,12 +1,14 @@
 package comjoshsibayan.github.alarm;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
-import android.widget.Toast;
 
 /**
  * Created by Josh on 8/31/2016.
@@ -55,6 +57,25 @@ public class RingtonePlayingService extends Service {
 
             this.isRunning = true;
             this.startId = 0;
+
+            // Notification service setup, set intent to link to MainActivity
+            NotificationManager notify_manager =
+                    (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+            Intent intent_main_activity = new Intent(this.getApplicationContext(), MainActivity.class);
+
+            // Create pending intent
+            PendingIntent pending_intent_main_activity =
+                    PendingIntent.getActivity(this, 0, intent_main_activity, 0);
+
+            // Set notification parameters
+            Notification notify_popup =
+                    new Notification.Builder(this).setContentTitle("An alarm is going off!")
+                            .setContentText("Click me!")
+                            .setContentIntent(pending_intent_main_activity)
+                            .setAutoCancel(true).build();
+
+            // Set notification call commands
+            notify_manager.notify(0, notify_popup);
 
         } else if(this.isRunning && startId == 0) {
             // If music playing and alarm off pressed, music stops
